586288b98b9a479916ff76df0098bf1480781f49
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-05 12:38:33 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 
-         $to = "igerpackersandmovers@gmail.com";
+         $to = "ligerpackersandmovers@gmail.com";
          $subject = "Liger Packers And Movers";
          
          $message = "<h1>Hi Iam ".$_POST['uname']." " .$_POST['uname1']."</h1>";
