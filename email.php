d6283bc4802ae9553f4117bf4c49df4488f443f8
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-12-05 10:06:28 +0530

    a
    a

---
+++ email.php
@@ -0,0 +1,22 @@
+<?php
+
+         $to = "pavan5639kumar@gmail.com";
+         $subject = "Liger Packers And Movers";
+         
+         $message = "<h1>Hi Iam ".$_POST['uname']." " .$_POST['uname1']."</h1>";
+         $message .= "<h2>Contact : <span style='color:blue;'>".$_POST['phone']."</span> </h2>";
+         $message .= "<b>".$_POST['message']."</b>";
+         
+         $header = "From:info@pkwebdev.com \r\n";
+         $header .= "Cc:info@pkwebdev.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
